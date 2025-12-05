aa840e28f1dcd9f4b04460e4fb96fbd2b8975392
Mikey-Ku <mkujr@olin.edu>
2025-12-05 13:25:29 -0500

    adding audio recorder

---
+++ audio_recorder.py
@@ -0,0 +1,13 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+duration = 5
+sample_rate = 44100
+
+print("Recording...")
+audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+sd.wait()
+print("Done recording!")
+
+write("output.wav", sample_rate, audio)
+print("Saved as output.wav")
